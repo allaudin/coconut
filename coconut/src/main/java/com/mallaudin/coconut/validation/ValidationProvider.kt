25e6c10c6c25422080ef94a2d05a5a7d0972b140
mallaudin <mallaudinqazi@gmail.com>
2018-10-29 11:01:10 +0500

    Add documentation for validation classes

--- coconut/src/main/java/com/mallaudin/coconut/validation/ValidationProvider.kt
+++ coconut/src/main/java/com/mallaudin/coconut/validation/ValidationProvider.kt
@@ -1,10 +1,22 @@
 package com.mallaudin.coconut.validation
 
-
+/**
+ * Validation provider interface for [com.mallaudin.coconut.widget.Input].
+ * It is responsible for providing a validator against a specific key
+ */
 interface ValidationProvider {
 
+    /**
+     * Provider initializer, responsible for collecting validators and
+     * saving them against respective keys
+     */
     fun init(): ValidationProvider
 
+    /**
+     * Providers [Validator] against key
+     *
+     * @return [Validator] for given key
+     */
     fun getByKey(key: String): Validator
 }
 
--- coconut/src/main/java/com/mallaudin/coconut/validation/ValidatorNotFound.kt
+++ coconut/src/main/java/com/mallaudin/coconut/validation/ValidatorNotFound.kt
@@ -1,3 +1,7 @@
 package com.mallaudin.coconut.validation
 
+/**
+ * Thrown when a key defined in layout does not match
+ * any [Validator] provided by [com.mallaudin.coconut.validation.ValidationProvider]
+ */
 class ValidatorNotFound(message: String) : RuntimeException(message)
